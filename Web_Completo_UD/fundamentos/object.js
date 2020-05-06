f2bdce6e49f5c69960309c09a3debbbe06933565
junior-rijo <alberino.rijo@outlook.com>
2020-05-06 17:59:49 -0300

    anotacoes gerais

--- Web_Completo_UD/fundamentos/object.js
+++ Web_Completo_UD/fundamentos/object.js
@@ -1,5 +1,14 @@
-const prod1 = {}
-prod1.nome = 'Celular Ultra'
+const prod1 = {}//Onjeto Vazio
+prod1.nome = 'Celular Ultra'//Comecei atribuir valores 
 prod1.preco = 500,00
 
-console.log(prod1) /*Criando Objectos Dinâmicos*/
+console.log(prod1) /*Criando Objectos Dinâmicos Ex.*/
+
+//Criando anotação literal criando atributo dentro do próprio objeto
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+//*//
+

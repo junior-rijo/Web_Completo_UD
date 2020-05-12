a13f90844961af3f237620ecd551d96c20dea017
junior-rijo <alberino.rijo@outlook.com>
2020-05-12 20:10:12 -0300

    null_undefined

---
+++ Web_Completo_UD/fundamentos/null_undefined.js
@@ -0,0 +1,17 @@
+/*
+const a = {name: "este"}//aponta para um endereço
+
+const b = a
+
+b.name = 'Opa'//mudando o b mudar o a
+*/
+//conceito de referencia
+let valor // não inicializada
+console.log(valor)
+
+valor = null//ausencia de valor
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)

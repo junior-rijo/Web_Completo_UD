e50ce2d4ceb0484a4dfe55233af8e0027d721a45
junior-rijo <alberino.rijo@outlook.com>
2020-08-04 21:57:03 -0300

    retomada de estudo

---
+++ Web_Completo_UD/controles/else.js
@@ -0,0 +1,29 @@
+/*const imprimirResultado = function (nota) {
+    if (nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+imprimirResultado(10)
+imprimirResultado(4)
+Number.prototype.entre = function (inicio, fim) {
+    return this >= inicio && this <= fim
+}
+const imprimirResultado = function (nota) {
+    if (nota.entre(9, 10)) {
+        console.log('Quadro de honra!')
+    } else if (nota.entre(7, 8.99)) {
+        console.log('Aprovado!')
+    } else if (nota.entre(0, 3.99)) {
+        console.log('Reprovado!')
+    } else {
+        console.log('fim!')
+    }
+
+}
+imprimirResultado(10)
+imprimirResultado(20)
+imprimirResultado(3)*/
+
+
--- Web_Completo_UD/fundamentos/func_vs_obtj.js
+++ Web_Completo_UD/fundamentos/func_vs_obtj.js
@@ -9,3 +9,4 @@ const Cliente = function() {}
 class Produto {} // ES 2015 (ES6)
 console.log(typeof Produto)
 console.log(typeof new Produto)
+

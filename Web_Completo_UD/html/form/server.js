c320d1c6008bf76f70638e7d0c32a73e79cf4e28
Alberino Junior <56556178+junior-rijo@users.noreply.github.com>
2020-09-08 17:41:54 -0300

    add_formaction

--- Web_Completo_UD/html/form/server.js
+++ Web_Completo_UD/html/form/server.js
@@ -7,7 +7,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) =>{
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns. Incluido com sucesso</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) =>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Alterado com Sucesso</h1>')
 })
 
 app.listen(3003)
